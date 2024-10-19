2958e87fd042721cf17321a1d259d4c5fa92f8d3
Anna Nahalka <nahalkaanna06@gmail.com>
2024-10-19 16:40:25 +0300

    PS-12 Create Premium page

--- PrepWise/app/src/main/java/com/example/prepwise/fragments/ProfileFragment.kt
+++ PrepWise/app/src/main/java/com/example/prepwise/fragments/ProfileFragment.kt
@@ -1,21 +1,26 @@
 package com.example.prepwise.fragments
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import androidx.navigation.fragment.findNavController
 import com.example.prepwise.LocaleHelper.loadLocale
 import com.example.prepwise.LocaleHelper.setLocale
 import com.example.prepwise.R
 import com.example.prepwise.activities.LoginActivity
+import com.example.prepwise.activities.PremiumActivity
 import java.util.Locale
 
 class ProfileFragment : Fragment() {
@@ -44,6 +49,38 @@ class ProfileFragment : Fragment() {
             requireActivity().finish()
         }
 
+        // Редагування профілю
+        val editProfile: ImageView = view.findViewById(R.id.edit)
+        editProfile.setOnClickListener{
+            val fragmentTransaction = requireActivity().supportFragmentManager.beginTransaction()
+            val targetFragment = EditProfileFragment()
+            fragmentTransaction.replace(R.id.fragment_container, targetFragment)
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
+
+        // Преміум
+        val goToPremium: LinearLayout = view.findViewById(R.id.premium)
+        goToPremium.setOnClickListener{
+            val intent = Intent(requireActivity(), PremiumActivity::class.java)
+            startActivity(intent)
+
+            requireActivity().finish()
+        }
+
+        // Лист із запитанням
+        val askQuestionLayout: LinearLayout = view.findViewById(R.id.ask_a_question)
+        askQuestionLayout.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("nahalkaanna06@gmail.com"))
+                putExtra(Intent.EXTRA_SUBJECT, "Запитання")
+            }
+            startActivity(emailIntent)
+        }
+
+
+
         return view
     }
 
@@ -52,7 +89,6 @@ class ProfileFragment : Fragment() {
 
         val mBuilder = AlertDialog.Builder(requireContext())
         mBuilder.setTitle(getString(R.string.choose_language))
-        // Використовуємо setItems замість setSingleChoiceItems
         mBuilder.setItems(listItems) { dialog, which ->
             when (which) {
                 0 -> {
